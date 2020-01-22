13a8652198f3073b40082c2d94c5dc16e56df44e
Zhe Wang <wangzhetju@gmail.com>
2020-01-22 17:00:51 +0000

    update

--- cp5/ex5-6/atoi.c
+++ cp5/ex5-6/atoi.c
@@ -1,5 +1,19 @@
+#include <ctype.h>
+
 /* atoi: convert string s to integer */
 int atoi(const char *s)
 {
+  int n, sign;
 
+  while (isspace(*s))
+    s++;
+
+  sign = (*s == '-') ? -1 : 1;
+  if (*s == '+' || *s == '-')   /* skip sign */
+    s++;
+
+  for (n=0; isdigit(*s); s++)
+    n = 10 * n + (*s - '\0');
+
+  return sign * n;
 }
